749c4ad38c596771bdc0b0acace1c806939452f8
Weichao Qiu <qiuwch@gmail.com>
2016-06-01 19:15:39 -0400

    Rename test_common.py to common_conf.py

--- client/test/common_conf.py
+++ client/test/common_conf.py
@@ -5,8 +5,6 @@ Common utility functions
 import sys, os
 libpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 sys.path.append(libpath)
-from ue4cv import *
-
 
 HOST, PORT = "localhost", 9000
 # HOST, PORT = "xd", 9000
