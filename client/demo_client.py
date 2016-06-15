6fd99bd68596f34f635c7cf849a87f9189521f35
Weichao Qiu <qiuwch@gmail.com>
2016-06-15 17:28:00 -0400

    Simplify demo_client script.

--- client/demo_client.py
+++ client/demo_client.py
@@ -1,26 +1,11 @@
 import ue4cv
 
-def subplot_image(index, image):
-    plt.subplot(index)
-    im = plt.imread(image)
-    plt.imshow(im)
-    plt.axis('off')
-
 ue4cv.client.connect()
 if not ue4cv.client.isconnected():
     print 'UnrealCV server is not running. Run the game downloaded from http://unrealcv.github.io first.'
 else:
-    filename = ue4cv.client.request('vget /camera/0/lit')
-    object_mask_filename = ue4cv.client.request('vget /camera/0/object_mask')
-    print 'Image saved to %s' % im
+    for mode in ['lit', 'depth', 'normal', 'object_mask']:
+        filename = ue4cv.client.request('vget /camera/0/%s' % mode)
+        print '%s image is saved to %s' % (mode, filename)
     ue4cv.client.disconnect()
 
-    try:
-        import matplotlib.pyplot as plt
-        subplot_image(121, filename)
-        subplot_image(122, object_mask_filename)
-        plt.show()
-
-    except Exception as e:
-        print 'matplotlib not installed'
-        print e
