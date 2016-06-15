7644bfd4922b0900b2812cae938f71db67de6d4f
Weichao Qiu <qiuwch@gmail.com>
2016-06-15 16:43:37 -0400

    Minor update to demo_client

--- client/demo_client.py
+++ client/demo_client.py
@@ -6,21 +6,19 @@ def subplot_image(index, image):
     plt.imshow(im)
     plt.axis('off')
 
-
-
 ue4cv.client.connect()
 if not ue4cv.client.isconnected():
-    print 'The game is not running, run it first'
+    print 'UnrealCV server is not running. Run the game downloaded from http://unrealcv.github.io first.'
 else:
-    im = ue4cv.client.request('vget /camera/0/lit')
-    object_mask = ue4cv.client.request('vget /camera/0/object_mask')
+    filename = ue4cv.client.request('vget /camera/0/lit')
+    object_mask_filename = ue4cv.client.request('vget /camera/0/object_mask')
     print 'Image saved to %s' % im
     ue4cv.client.disconnect()
 
     try:
         import matplotlib.pyplot as plt
-        subplot_image(121, im)
-        subplot_image(122, object_mask)
+        subplot_image(121, filename)
+        subplot_image(122, object_mask_filename)
         plt.show()
 
     except Exception as e:
