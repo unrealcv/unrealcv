068f26f9cb6fe57a55fa515ce55d91fc9a26275c
Weichao Qiu <qiuwch@gmail.com>
2016-08-08 13:01:53 -0400

    Update plugin build script.

--- build-plugin.py
+++ build-plugin.py
@@ -1,5 +1,9 @@
 import os
-UnrealEnginePath='/home/qiuwch/workspace/UnrealEngine'
-UATScript = os.path.join(UnrealEnginePath, 'Engine/Build/BatchFiles/RunUAT.sh')
+UnrealEnginePath='~/workspace/UnrealEngine/Engine'
+
+UATScript = os.path.join(UnrealEnginePath, 'Build/BatchFiles/RunUAT.sh')
+if not os.path.isfile(UATScript):
+    print('Can not find Automation Script of UE4 %s' % UATScript)
+    print('Please set UnrealEnginePath correctly first')
 FullPluginFile = os.path.abspath('UnrealCV.uplugin')
 os.system('%s BuildPlugin -plugin=%s' % (UATScript, FullPluginFile))
