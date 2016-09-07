3de2bd7a93f10e8ca1a016a892a4aac2e9ebd25c
Weichao Qiu <qiuwch@gmail.com>
2016-09-07 17:29:04 -0400

    Update python client demo.

--- client/python/demo.py
+++ client/python/demo.py
@@ -1,23 +1,13 @@
-from unrealcv import Client
-client = Client(('10.161.159.75', 9000))
-# Try to connect our python client to the game
-client.connect()
-
-# Check if the connection is successfully established
-if not client.isconnected():
-    print 'UnrealCV server is not running. Run the game downloaded from http://unrealcv.github.io first.'
+from unrealcv import client
+client.connect() # Connect to the game
+if not client.isconnected(): # Check if the connection is successfully established
+    print 'UnrealCV server is not running. Run the game from http://unrealcv.github.io first.'
 else:
     filename = client.request('vget /camera/0/lit')
     print 'Image is saved to %s' % filename
-
-    filename = client.request('vget /camera/0/depth')
-    print 'Depth is saved to %s' % filename
-
-    filename = client.request('vget /camera/0/object_mask')
-    print 'Object instance mask is saved to %s' % filename
-
-    filename = client.request('vget /camera/0/normal')
-    print 'Surface normal is saved to %s' % filename
-
-    # Disconnect python client from the game
-    client.disconnect()
+    for gt_type in ['normal', 'object_mask']:
+        filename = client.request('vget /camera/0/%s' % gt_type)
+        print '%s is saved to %s' % (gt_type, filename)
+    filename = client.request('vget /camera/0/depth depth.exr')
+    print 'depth is saved to %s' % filename
+    # Depth needs to be saved to HDR image to ensure numerical accuracy
