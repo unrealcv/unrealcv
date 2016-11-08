4df7526ac3e22e027804f2c19fb1cc41276d7fd6
Weichao Qiu <qiuwch@gmail.com>
2016-11-07 22:05:21 -0500

    Fix multi-LOD support.

--- Source/UnrealCV/Private/ObjectPainter.cpp
+++ Source/UnrealCV/Private/ObjectPainter.cpp
@@ -279,58 +279,58 @@ bool FObjectPainter::PaintObject(AActor* Actor, const FColor& Color, bool IsColo
 		{
 			if (UStaticMesh* StaticMesh = StaticMeshComponent->StaticMesh)
 			{
-				uint32 PaintingMeshLODIndex = 0;
 				uint32 NumLODLevel = StaticMeshComponent->StaticMesh->RenderData->LODResources.Num();
-				check(NumLODLevel == 1);
-				FStaticMeshLODResources& LODModel = StaticMeshComponent->StaticMesh->RenderData->LODResources[PaintingMeshLODIndex];
-				FStaticMeshComponentLODInfo* InstanceMeshLODInfo = NULL;
-
-				// PaintingMeshLODIndex + 1 is the minimum requirement, enlarge if not satisfied
-				StaticMeshComponent->SetLODDataCount(PaintingMeshLODIndex + 1, StaticMeshComponent->LODData.Num());
-				InstanceMeshLODInfo = &StaticMeshComponent->LODData[PaintingMeshLODIndex];
-
-				// Setup OverrideVertexColors
-				// if (!InstanceMeshLODInfo->OverrideVertexColors) // TODO: Check this
+				for (uint32 PaintingMeshLODIndex = 0; PaintingMeshLODIndex < NumLODLevel; PaintingMeshLODIndex++)
 				{
-					InstanceMeshLODInfo->OverrideVertexColors = new FColorVertexBuffer;
+					FStaticMeshLODResources& LODModel = StaticMeshComponent->StaticMesh->RenderData->LODResources[PaintingMeshLODIndex];
+					FStaticMeshComponentLODInfo* InstanceMeshLODInfo = NULL;
 
-					FColor FillColor = FColor(255, 255, 255, 255);
-					InstanceMeshLODInfo->OverrideVertexColors->InitFromSingleColor(FColor::White, LODModel.GetNumVertices());
+					// PaintingMeshLODIndex + 1 is the minimum requirement, enlarge if not satisfied
+					StaticMeshComponent->SetLODDataCount(PaintingMeshLODIndex + 1, StaticMeshComponent->LODData.Num());
+					InstanceMeshLODInfo = &StaticMeshComponent->LODData[PaintingMeshLODIndex];
+
+					// Setup OverrideVertexColors
+					// if (!InstanceMeshLODInfo->OverrideVertexColors) // TODO: Check this
+					{
+						InstanceMeshLODInfo->OverrideVertexColors = new FColorVertexBuffer;
+
+						FColor FillColor = FColor(255, 255, 255, 255);
+						InstanceMeshLODInfo->OverrideVertexColors->InitFromSingleColor(FColor::White, LODModel.GetNumVertices());
+					}
+
+					uint32 NumVertices = LODModel.GetNumVertices();
+					check(InstanceMeshLODInfo->OverrideVertexColors);
+					check(NumVertices <= InstanceMeshLODInfo->OverrideVertexColors->GetNumVertices());
+					// StaticMeshComponent->CachePaintedDataIfNecessary();
+
+					for (uint32 ColorIndex = 0; ColorIndex < NumVertices; ++ColorIndex)
+					{
+						// LODModel.ColorVertexBuffer.VertexColor(ColorIndex) = NewColor;  // This is vertex level
+						// Need to initialize the vertex buffer first
+						uint32 NumOverrideVertexColors = InstanceMeshLODInfo->OverrideVertexColors->GetNumVertices();
+						uint32 NumPaintedVertices = InstanceMeshLODInfo->PaintedVertices.Num();
+						// check(NumOverrideVertexColors == NumPaintedVertices);
+						InstanceMeshLODInfo->OverrideVertexColors->VertexColor(ColorIndex) = NewColor;
+						// InstanceMeshLODInfo->PaintedVertices[ColorIndex].Color = NewColor;
+					}
+					BeginInitResource(InstanceMeshLODInfo->OverrideVertexColors);
+					StaticMeshComponent->MarkRenderStateDirty();
+					// BeginUpdateResourceRHI(InstanceMeshLODInfo->OverrideVertexColors);
+
+
+					/*
+					// TODO: Need to check other LOD levels
+					// Use flood fill to paint mesh vertices
+					UE_LOG(LogUnrealCV, Warning, TEXT("%s:%s has %d vertices"), *Actor->GetActorLabel(), *StaticMeshComponent->GetName(), NumVertices);
+
+					if (LODModel.ColorVertexBuffer.GetNumVertices() == 0)
+					{
+						// Mesh doesn't have a color vertex buffer yet!  We'll create one now.
+						LODModel.ColorVertexBuffer.InitFromSingleColor(FColor(255, 255, 255, 255), LODModel.GetNumVertices());
+					}
+
+					*/
 				}
-
-				uint32 NumVertices = LODModel.GetNumVertices();
-				check(InstanceMeshLODInfo->OverrideVertexColors);
-				check(NumVertices <= InstanceMeshLODInfo->OverrideVertexColors->GetNumVertices());
-				// StaticMeshComponent->CachePaintedDataIfNecessary();
-
-				for (uint32 ColorIndex = 0; ColorIndex < NumVertices; ++ColorIndex)
-				{
-					// FColor NewColor = FColor(FMath::RandRange(0, 255), FMath::RandRange(0, 255), FMath::RandRange(0, 255), 255);
-					// LODModel.ColorVertexBuffer.VertexColor(ColorIndex) = NewColor;  // This is vertex level
-					// Need to initialize the vertex buffer first
-					uint32 NumOverrideVertexColors = InstanceMeshLODInfo->OverrideVertexColors->GetNumVertices();
-					uint32 NumPaintedVertices = InstanceMeshLODInfo->PaintedVertices.Num();
-					// check(NumOverrideVertexColors == NumPaintedVertices);
-					InstanceMeshLODInfo->OverrideVertexColors->VertexColor(ColorIndex) = NewColor;
-					// InstanceMeshLODInfo->PaintedVertices[ColorIndex].Color = NewColor;
-				}
-				BeginInitResource(InstanceMeshLODInfo->OverrideVertexColors);
-				StaticMeshComponent->MarkRenderStateDirty();
-				// BeginUpdateResourceRHI(InstanceMeshLODInfo->OverrideVertexColors);
-
-
-				/*
-				// TODO: Need to check other LOD levels
-				// Use flood fill to paint mesh vertices
-				UE_LOG(LogUnrealCV, Warning, TEXT("%s:%s has %d vertices"), *Actor->GetActorLabel(), *StaticMeshComponent->GetName(), NumVertices);
-
-				if (LODModel.ColorVertexBuffer.GetNumVertices() == 0)
-				{
-					// Mesh doesn't have a color vertex buffer yet!  We'll create one now.
-					LODModel.ColorVertexBuffer.InitFromSingleColor(FColor(255, 255, 255, 255), LODModel.GetNumVertices());
-				}
-
-				*/
 			}
 		}
 	}
