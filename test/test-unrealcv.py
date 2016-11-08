b65c95ca400f91648a53553f51979f5ceb7a0d94
Weichao Qiu <qiuwch@gmail.com>
2016-11-08 17:15:49 -0500

    Clean up the portal of test script.

--- test/test-unrealcv.py
+++ test/test-unrealcv.py
@@ -1,21 +1,35 @@
-# TODO: Test robustness, test speed
+'''
+Run python test.py to test unrealcv
+'''
 import unittest, time, sys, argparse, threading
-sys.path.append('./test/ipc')
 from common_conf import *
-from test_dev_server import TestDevServer
-from test_client import TestUE4CVClient
+
+def run_full_test():
+    pass
+
+def run_travis_test():
+    sys.path.append('./test/ipc')
+    from test_dev_server import TestDevServer
+    from test_client import TestUE4CVClient
+    load = unittest.TestLoader().loadTestsFromTestCase
+
+    suites = []
+    s = load(TestDevServer); suites.append(s)
+    s = load(TestUE4CVClient); suites.append(s)
+    suite_obj = unittest.TestSuite(suites)
+
+    ret = not unittest.TextTestRunner(verbosity = 2).run(suite_obj).wasSuccessful()
+
+    return ret
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--travis', action='store_true') # Only run test availabe to travis CI
 
     args = parser.parse_args()
-    suites = []
+    if args.travis:
+        ret = run_travis_test()
+    else:
+        ret = run_full_test()
 
-    load = unittest.TestLoader().loadTestsFromTestCase
-    s = load(TestDevServer); suites.append(s)
-    s = load(TestUE4CVClient); suites.append(s)
-
-    suite_obj = unittest.TestSuite(suites)
-    ret = not unittest.TextTestRunner(verbosity = 2).run(suite_obj).wasSuccessful()
     sys.exit(ret)
