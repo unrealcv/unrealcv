350d8897a432adf048cdf7fdbb99ec49acf89af0
Weichao Qiu <qiuwch@gmail.com>
2016-12-15 17:34:13 -0500

    Minor update to build.bat.

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-REM set the environment variable in console or in system variables UE4=D:\Epic Games\4.13\Engine\
+@echo Set the environment variable UE4 in the console or in the system variables
+@echo For example in my machine, I use "set UE4=D:\Epic Games\4.13\Engine\"
 "%UE4%/Build/BatchFiles/RunUAT.bat" BuildPlugin -plugin=%CD%/UnrealCV.uplugin -package=%CD%/built -rocket -targetplatforms=Win64
