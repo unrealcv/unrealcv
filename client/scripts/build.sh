bfd47dd01f25503f8ec1d6b8bf86f6895f953ab8
Weichao Qiu <qiuwch@gmail.com>
2017-01-06 23:41:36 +0800

    Remove unneccessary python scripts.

--- client/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-project_file=$1
-plugin_version=$(git -C . rev-parse --short HEAD)
-echo "Build plugin"
-python build-plugin.py
-echo "Install plugin to project"
-python install-plugin.py ${plugin_version} ${project_file}
-echo "Build project"
-python build-project.py ${project_file}
