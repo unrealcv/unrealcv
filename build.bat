fcea095fbe44f61b937a2a09ab6f0b20176f59ef
Weichao Qiu <qiuwch@gmail.com>
2017-03-03 20:03:38 -0500

    Update output folder of the build script.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 set plugin_file=%CD%/UnrealCV.uplugin
-set package_folder=%CD%/built
+set package_folder=%CD%/Plugins/UnrealCV
 set UE4=C:\Program Files\Epic Games\UE_4.14
 REM TODO: Check the existence of UE4 folder and give a useful hint for user
 REM Modify the UE4 variable to point to your UE4 installation path
