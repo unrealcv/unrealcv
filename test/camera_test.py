94bd8b87c2c8930b7286d2561b3880460d1b9bfa
Weichao Qiu <qiuwch@gmail.com>
2017-05-26 01:45:01 -0400

    Add test for binary mode.

--- test/camera_test.py
+++ test/camera_test.py
@@ -1,6 +1,7 @@
 from unrealcv import client
 from conftest import env
 import cv2
+from PIL import Image
 
 def test_gt(env):
     client.connect()
@@ -12,3 +13,44 @@ def test_gt(env):
         print(im.shape)
 
     # Make sure the dimension is right
+
+def imread_binary(res):
+    import StringIO
+    import numpy as np
+    PILimg = Image.open(StringIO.StringIO(res))
+    img = np.array(PILimg)
+    return img
+
+def imread(res):
+    return cv2.imread(res)
+
+def test_binary_mode(env):
+    client.connect()
+    cmds = [
+        'vget /camera/0/lit png',
+        'vget /camera/0/object_mask png',
+        'vget /camera/0/normal png',
+    ]
+    for cmd in cmds:
+        print('Test %s' % cmd)
+        res = client.request(cmd)
+        print(len(res))
+        im = imread_binary(res)
+        print im.shape
+
+    # TODO: add assert
+
+def test_file_mode(env):
+    client.connect()
+    print client.request('vget /unrealcv/status')
+    res = client.request('vget /camera/0/lit test.png')
+    im = imread(res)
+    print im.shape
+
+    res = client.request('vget /camera/0/object_mask test.png')
+    im = imread(res)
+    print im.shape
+
+if __name__ == '__main__':
+    test_binary_mode(None)
+    # test_file_mode(None)
