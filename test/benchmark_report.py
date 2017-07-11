42c45dfe3e6c83ccd8a094bed1da815631c516b1
Weichao Qiu <qiuwch@gmail.com>
2017-07-10 22:24:39 -0400

    Update benchmark_report.

--- test/benchmark_report.py
+++ test/benchmark_report.py
@@ -4,31 +4,82 @@ Check whether it is fast enough for your application
 '''
 from __future__ import print_function
 from unrealcv import client
-import docker_util
-import time
-import pytest
+import time, json, argparse
 
-def run_command(cmd, num):
-    for _ in range(num):
-        client.request(cmd)
+def run_count(cmd, count):
+    tic = time.time()
+    for i in range(count):
+        res = client.request(cmd)
+        if i == 0:
+            sample_output = res
+
+    toc = time.time()
+    elapse = toc - tic
+    msg = 'Run %s for %d times, time = %.2f, %d FPS' \
+        % (cmd, num, elapse, float(num) / elapse)
+    return msg, sample_output
+
+def run_duration(cmd, duration):
+    tic = time.time()
+    for i in range(100000):
+        elapse = time.time() - tic # A little overhead here
+        if elapse > duration:
+            count = i
+            break
+
+        res = client.request(cmd)
+        if i == 0:
+            sample_output = res
+
+    msg = 'Run %s for %0.2f seconds, count = %d, %d FPS' \
+        % (cmd, duration, count, float(count) / duration)
+    return msg, sample_output
+
+def main():
+    def log(msg):
+        # no need to use logging module for such a simple use case
+        print(msg)
+        report_file.write(msg + '\n')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', default='benchmark_task.json', help = 'A json file to define the task')
+    parser.add_argument('--report', default='benchmark_report.txt')
+
+    args = parser.parse_args()
+    json_task = args.task
+    report_filename = args.report
+
+    try:
+        data = json.load(open(json_task))
+    except Exception as e:
+        print('Failed to parse task file %s' % json_task)
+        print(e)
+        return
+    tasks = data.get('Tasks')
+
+    report_file = open(report_filename, 'w')
+
+    client.connect()
+    for task in tasks:
+        task_text = str(task)
+        if task.get('Cmd') == None or task.get('Skip') == True:
+            continue
+
+        if task.get('Duration'):
+            duration = task.get('Duration')
+            task_result, sample_output = run_duration(task['Cmd'], duration)
+
+        if task.get('Count'):
+            count = task.get('Count')
+            task_result, sample_output = run_count(task['Cmd'], count)
+
+        sample_output = sample_output.split('\n')[0][:256]
+        log(task_text)
+        log(task_result)
+        log(sample_output)
+        print('-' * 80)
+
+    report_file.close()
 
 if __name__ == '__main__':
-    print('Start docker')
-    # Initialize the environment
-    docker_util.runner.start()
-    client.connect()
-
-    commands = [
-        ('vget /unrealcv/status', 1000),
-        ('vset /unrealcv/sync test message', 1000),
-        ('vget /camera/0/lit', 100),
-    ]
-
-    for (cmd, num) in commands:
-        tic = time.time()
-        run_command(cmd, num)
-        toc = time.time()
-        elapse = toc - tic
-        print('Run %s for %d, time = %.2f, %d FPS' % (cmd, num, elapse, float(num) / elapse))
-
-    docker_util.runner.stop()
+    main()
