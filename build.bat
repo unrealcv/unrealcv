b2ea4fc891b1542cb8016d72538d1f8963a4c591
Weichao Qiu <qiuwch@gmail.com>
2017-10-12 22:06:55 -0400

    Remove old build scripts, use python exclusively.

--- build.bat
+++
@@ -1,6 +0,0 @@
-set plugin_file=%CD%/UnrealCV.uplugin
-set package_folder=%CD%/Plugins/UnrealCV
-REM TODO: Check the existence of UE4 folder and give a useful hint for user
-REM Modify the UE4 variable to point to your UE4 installation path
-"%UE4%/Engine/Build/BatchFiles/RunUAT.bat" BuildPlugin -plugin=%plugin_file% -package=%package_folder% -rocket -targetplatforms=Win64
-xcopy /E /Y Plugins\UnrealCV\Binaries %CD%
