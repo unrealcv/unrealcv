f5d56b0c54af414f02721a1a02a0eaf80dbba898
Weichao Qiu <qiuwch@gmail.com>
2017-10-15 22:56:34 -0400

    Handle exceptions in read_png and read_npy.

--- client/python/unrealcv/util.py
+++ client/python/unrealcv/util.py
@@ -1,13 +1,22 @@
 import numpy as np
-import PIL
+import PIL.Image
 from io import BytesIO
 # StringIO module is removed in python3, use io module
 
 def read_png(res):
-    import PIL.Image
-    img = PIL.Image.open(BytesIO(res))
-    return np.asarray(img)
+    img = None
+    try:
+        PIL_img = PIL.Image.open(BytesIO(res))
+        img = np.asarray(PIL_img)
+    except:
+        print('Read png can not parse response %s' % str(res[:20]))
+    return img
 
 def read_npy(res):
     # res is a binary buffer
-    return np.load(BytesIO(res))
+    arr = None
+    try:
+        arr = np.load(BytesIO(res))
+    except:
+        print('Read npy can not parse response %s' % str(res[:20]))
+    return arr
