d6c7dee9ce25e4db8f5a8ecacf286ff57415c165
Weichao Qiu <qiuwch@gmail.com>
2017-10-15 22:55:29 -0400

    Add example scripts about packaging binaries.

---
+++ examples/model_zoo/build_ue416_win_binaries.bat
@@ -0,0 +1,5 @@
+python build.py --UE4 "C:\Program Files\Epic Games\UE_4.16" C:\qiuwch\workspace\uprojects\UE4ArchinteriorsVol2Scene1\ArchinteriorsVol2Scene1.uproject | tee log/scene1.log
+python build.py --UE4 "C:\Program Files\Epic Games\UE_4.16" C:\qiuwch\workspace\uprojects\UE4ArchinteriorsVol2Scene2\ArchinteriorsVol2Scene2.uproject | tee log/scene2.log
+python build.py --UE4 "C:\Program Files\Epic Games\UE_4.16" C:\qiuwch\workspace\uprojects\UE4ArchinteriorsVol2Scene3\ArchinteriorsVol2Scene3.uproject | tee log/scene3.log
+python build.py --UE4 "C:\Program Files\Epic Games\UE_4.16" C:\qiuwch\workspace\uprojects\UE4RealisticRendering\RealisticRendering.uproject  | tee log/rr.log
+python build.py --UE4 "C:\Program Files\Epic Games\UE_4.16"  C:\qiuwch\workspace\uprojects\UE4UrbanCity\UrbanCity.uproject  | tee log/urbancity.log
---
+++ examples/model_zoo/test_binaries.bat
@@ -0,0 +1,5 @@
+python docs/reference/commands_demo.py D:/unrealcv/Binaries/ArchinteriorsVol2Scene1/WindowsNoEditor/ArchinteriorsVol2Scene1.exe --output_folder output/arch1
+python docs/reference/commands_demo.py D:/unrealcv/Binaries/ArchinteriorsVol2Scene2/WindowsNoEditor/ArchinteriorsVol2Scene2.exe --output_folder output/arch2
+python docs/reference/commands_demo.py D:/unrealcv/Binaries/ArchinteriorsVol2Scene3/WindowsNoEditor/ArchinteriorsVol2Scene3.exe --output_folder output/arch3
+python docs/reference/commands_demo.py D:/unrealcv/Binaries/UrbanCity/WindowsNoEditor/UrbanCity.exe --output_folder output/urbancity
+python docs/reference/commands_demo.py D:/unrealcv/Binaries/RealisticRendering/WindowsNoEditor/RealisticRendering.exe --output_folder output/rr
