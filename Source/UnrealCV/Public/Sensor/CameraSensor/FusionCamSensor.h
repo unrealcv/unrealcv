554d0ccbf2fb7c7f1a5557cc8e3f5b2591148aea
Weichao Qiu <qiuwch@gmail.com>
2018-10-05 20:27:40 -0400

    Fix camera 0 issue.

--- Source/UnrealCV/Public/Sensor/CameraSensor/FusionCamSensor.h
+++ Source/UnrealCV/Public/Sensor/CameraSensor/FusionCamSensor.h
@@ -132,7 +132,7 @@ public:
 	virtual void PostEditChangeProperty(FPropertyChangedEvent &PropertyChangedEvent) override;
 #endif
 
-private:
+protected:
 	UPROPERTY()
 	TArray<class UBaseCameraSensor*> FusionSensors;
 
