18c05eb1009ad318217aa266b6883f5cc6306f8e
vlordier <vincent.lordier@sanofi.com>
2026-03-03 23:14:03 +0100

    Address Copilot feedback on CommandDispatcher includes

--- Source/UnrealCV/Private/Server/CommandDispatcher.cpp
+++ Source/UnrealCV/Private/Server/CommandDispatcher.cpp
@@ -1,9 +1,5 @@
 // Weichao Qiu @ 2016
 #include "CommandDispatcher.h"
-#include "HAL/Runnable.h"
-#include "HAL/RunnableThread.h"
-#include "Async/Async.h"
-#include "Containers/Queue.h"
 #include "Internationalization/Regex.h"
 #include "UnrealcvStats.h"
 #include "UnrealcvLog.h"
