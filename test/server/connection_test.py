76ef5ddfc1435ee31dc9204b50ca99a02c92812a
vlordier <vincent.lordier@sanofi.com>
2026-03-05 07:33:13 +0100

    tests(server): add explicit timeouts to prevent indefinite request waits

--- test/server/connection_test.py
+++ test/server/connection_test.py
@@ -1,25 +1,29 @@
 import unrealcv
 from unrealcv import client
+
 # from docker_util import docker_runner
 import pytest
 from conftest import checker
 
+
 def test_connection():
-    client.connect()
-    res = client.request('vget /unrealcv/status')
+    client.connect(timeout=2)
+    res = client.request("vget /unrealcv/status", timeout=5)
     assert checker.not_error(res)
     # assert res == 'ok'
 
+
 def test_echo():
-    client.connect()
-    res = client.request('vget /unrealcv/echo test')
-    assert res == 'test'
+    client.connect(timeout=2)
+    res = client.request("vget /unrealcv/echo test", timeout=5)
+    assert res == "test"
+
 
 def test_viewmode():
-    viewmodes = ['lit', 'depth', 'object_mask', 'normal']
+    viewmodes = ["lit", "depth", "object_mask", "normal"]
     for viewmode in viewmodes:
-        cmd = 'vset /viewmode {viewmode}'.format(viewmode = viewmode)
+        cmd = "vset /viewmode {viewmode}".format(viewmode=viewmode)
         print(cmd)
-        res = client.request(cmd)
+        res = client.request(cmd, timeout=5)
         assert checker.is_ok(res)
     client.disconnect()
--- test/server/object_test.py
+++ test/server/object_test.py
@@ -1,23 +1,27 @@
-'''
+"""
 Test object related functions
-'''
+"""
+
 import pytest
 from unrealcv import client
 from conftest import checker
 
+
 def test_object_list():
-    client.connect()
-    res = client.request('vget /objects')
+    client.connect(timeout=2)
+    res = client.request("vget /objects", timeout=5)
     if checker.is_error(res):
-        pytest.skip('Object listing is not available in this host project')
-    obj_ids = res.split(' ')
+        pytest.skip("Object listing is not available in this host project")
+    obj_ids = res.split(" ")
     assert checker.not_error(res)
 
     for obj_id in obj_ids:
-        color = client.request('vget /object/%s/color' % obj_id)
+        color = client.request("vget /object/%s/color" % obj_id, timeout=5)
         if isinstance(color, str) and (
-            'Cannot find object' in color or color.startswith('error Not Implemented')
+            "Cannot find object" in color or color.startswith("error Not Implemented")
         ):
-            pytest.skip('Object color query not supported for this host project content')
+            pytest.skip(
+                "Object color query not supported for this host project content"
+            )
         assert checker.not_error(color)
     client.disconnect()
