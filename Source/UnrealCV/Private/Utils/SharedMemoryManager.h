c5ffd53a30100393a565f9bb740d4d622252c9d3
lizi-Margin <sunhaocheng@bupt.edu.cn>
2026-08-09 02:17:52 +0800

    Revert "Format shared-memory transport sources"
    This reverts commit 4d9d7e35f51835032dc868aa714ce2ee649e70cc.

--- Source/UnrealCV/Private/Utils/SharedMemoryManager.h
+++ Source/UnrealCV/Private/Utils/SharedMemoryManager.h
@@ -4,33 +4,32 @@
 
 struct FUnrealCVSharedMemoryView
 {
-    FString Name;
-    int64 NumBytes = 0;
-    uint64 Version = 0;
-    int32 OffsetBytes = 0;
-    void* Data = nullptr;
-    double MapMs = 0.0;
-    double MemcpyMs = 0.0;
-    double TotalMs = 0.0;
+	FString Name;
+	int64 NumBytes = 0;
+	uint64 Version = 0;
+	int32 OffsetBytes = 0;
+	void* Data = nullptr;
+	double MapMs = 0.0;
+	double MemcpyMs = 0.0;
+	double TotalMs = 0.0;
 };
 
 class FUnrealCVSharedMemoryManager
 {
-  public:
-    static FUnrealCVSharedMemoryManager& Get();
+public:
+	static FUnrealCVSharedMemoryManager& Get();
 
-    bool WriteBytes(const FString& LogicalName, const void* SourceData, int64 NumBytes,
-                    FUnrealCVSharedMemoryView& OutView, FString& OutError);
-    bool Release(const FString& LogicalName);
+	bool WriteBytes(const FString& LogicalName, const void* SourceData, int64 NumBytes, FUnrealCVSharedMemoryView& OutView, FString& OutError);
+	bool Release(const FString& LogicalName);
 
-  private:
-    struct FSharedMemoryRegion;
+private:
+	struct FSharedMemoryRegion;
 
-    FString MakePlatformName(const FString& LogicalName, uint64 Version) const;
-    bool CreateOrResizeRegion(const FString& LogicalName, int64 NumBytes, FUnrealCVSharedMemoryView& OutView,
-                              FString& OutError);
+	FString MakePlatformName(const FString& LogicalName, uint64 Version) const;
+	bool CreateOrResizeRegion(const FString& LogicalName, int64 NumBytes, FUnrealCVSharedMemoryView& OutView, FString& OutError);
 
-    FCriticalSection CriticalSection;
-    TMap<FString, TUniquePtr<FSharedMemoryRegion>> Regions;
-    TMap<FString, uint64> RegionVersions;
+	FCriticalSection CriticalSection;
+	TMap<FString, TUniquePtr<FSharedMemoryRegion>> Regions;
+	TMap<FString, uint64> RegionVersions;
 };
+
